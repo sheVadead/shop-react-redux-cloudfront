fb23660b5450b6878b2cafbd77599b75a8b2fd21
Dzmitry Shauchonak <dzmitry_shauchonak@epam.com>
2022-07-29 12:39:22 +0200

    feat: add import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   products: 'https://1bayz4m1hc.execute-api.eu-central-1.amazonaws.com/dev/products',
   productsById: 'https://1bayz4m1hc.execute-api.eu-central-1.amazonaws.com/dev/products/{id}',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1bayz4m1hc.execute-api.eu-central-1.amazonaws.com/dev/products',
+  import: 'https://jvt21vcmc3.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
